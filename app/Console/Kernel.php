5132a7fcd703b7ef6874d3ca4975f4e76fb75ce4
tokki <se7enmail@gmail.com>
2020-03-12 11:38:58 +0900

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('store:scraping --force')->everyFiveMinutes();
+        $schedule->command('store:scraping')->everyFiveMinutes();
     }
 
     /**
